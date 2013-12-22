f720cf1c49e61b448eae1f59947b65dcd300c88c
John Cotham <jcotham@utk.edu>
2013-12-21 19:42:57 -0500

    updated model

--- questions/models.py
+++ questions/models.py
@@ -15,29 +15,29 @@ class QuestionChain(models.Model):
 
 class Question(models.Model):
   question_type = models.CharField(max_length=5)
-  display_key = models.CharField(max_length=30)
   question_text = models.CharField(max_length=300)
-  display_form = models.CharField(max_length=100)
+  display_text = models.CharField(max_length=100)
+  display_group = models.CharField(max_length=30)
 
   def __str__(self):
     return self.question_text
 
 class Option(models.Model):
   question = models.ForeignKey(Question)
-  branch_y = models.ForeignKey(QuestionChain)
+  branch_id = models.ForeignKey(QuestionChain)
   text = models.CharField(max_length=50)
-  display_form = models.CharField(max_length=10)
+  display_text = models.CharField(max_length=10)
+  highlight = models.CharField(max_length=2)
 
   def __str__(self):
     return self.text
 
 class ChainToQuestion(models.Model):
   question = models.ForeignKey(Question)
-  chain = models.ForeignKey(QuestionChain)
-  position = models.IntegerField()
+  chain_id = models.ForeignKey(QuestionChain)
+  chain_index = models.IntegerField()
   
 class QuestionSetToChain(models.Model):
   question_set = models.ForeignKey(QuestionSet)
   question_chain = models.ForeignKey(QuestionChain)
-  position = models.IntegerField()
-  stack = models.BooleanField()
+  stack_index = models.IntegerField()
